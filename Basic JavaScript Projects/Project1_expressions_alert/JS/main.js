a990616a4fb9433f89a25676bfc331e0c6d48864
Arreclain <arreclain@gmail.com>
2025-05-20 13:39:06 -0700

    Updated the challenge document.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,6 +2,11 @@ var A = "Hello World!!";   // A string.  Designed to add to another string in a 
 var B = "\"This string\'ll contain backslashes!\" he said.";  //A string to be used in an alert box and to demonstrate escaping characters. 
 var C = "  The world needs a hug."; // A string.  Designed to add to another string in a doc.write.
 var x = 1, y = 2, z = 3;  //I assign 3 variables, x, y, and z.  They get the values 1, 2, and 3 respectively.
+var send1 = "This is the beginning of the string";
+var send2 =" and this is the end of the string.";
+
+//Actual code here.
 window.alert(B);
 document.write(A+C);  //sums two strings.
 document.write(z+y);  //sums two numeric values.
+document.write(send1+send2);
