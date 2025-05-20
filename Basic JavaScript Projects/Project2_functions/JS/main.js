199a5887591671423375613b7464ee189fe89582
Arreclain <arreclain@gmail.com>
2025-05-20 15:13:58 -0700

    Fixed the function to do what was desired.

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,12 +1,12 @@
 var var1 = "Our powers combined";
 var var2 = " create one complete sentence!";
 var lure = "Hover your mouse here...";
-var springTheTrap ="and more text will appear!";
+var springTheTrap ="and yet more text will appear...";
 
  function changeName() {
     document.getElementById("JS-Target").innerHTML = var1+var2;
  }
 
  function concatonate(){
-    document.getElementById("JS-Hover").innerHTML = lure + springTheTrap;
+    document.getElementById("JS-Hover").innerHTML += springTheTrap;
  }
