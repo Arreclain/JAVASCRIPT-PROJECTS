f33389c89984d20e9e706a4832a39ffdd10dc81a
Arreclain <arreclain@gmail.com>
2025-05-22 16:19:51 -0700

    I fixed a typo that was important.

--- Basic JavaScript Projects/Project5_comparisons_type_coersion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coersion/JS/main.js
@@ -1,30 +1,50 @@
-function coercion(){
-let x = "Help";
-let y = 7;
-let a = x+y;
-let b = y+x;
-document.getElementById("aResults").innerHTML = "String plus a number yields a " + typeof a;
-document.getElementById("bResults").innerHTML = "A number plus a string yields a " + typeof b;
-//document.write(typeof a);
-//document.write(typeof b);
+function coercion() {
+    let x = "Help";
+    let y = 7;
+    let a = x + y;
+    let b = y + x;
+    document.getElementById("aResults").innerHTML = "String plus a number yields a " + typeof a;
+    document.getElementById("bResults").innerHTML = "A number plus a string yields a " + typeof b;
+    //document.write(typeof a);
+    //document.write(typeof b);
 
-document.getElementById("naanIsBread").innerHTML = 0/0;
-document.getElementById("correct").innerHTML = isNaN("Run");
-document.getElementById("incorrect").innerHTML = isNaN(3);
+    document.getElementById("naanIsBread").innerHTML = 0 / 0;
+    document.getElementById("correct").innerHTML = isNaN("Run");
+    document.getElementById("incorrect").innerHTML = isNaN(3);
 
-document.getElementById("toInfinity").innerHTML = 3E320;
-document.getElementById("andBeyond").innerHTML = -3E320;
+    document.getElementById("toInfinity").innerHTML = 3E320;
+    document.getElementById("andBeyond").innerHTML = -3E320;
 }
 
-function booleans(){
+function booleans() {
     let x = 16;
     part1 = false;
     part2 = false;
     reply = "Well..."
 
-    reply+= (x%2 == 0);
-    reply+=" and "
-    reply+= (x>20);
-    reply+="."
+    reply += (x % 2 == 0);
+    reply += " and "
+    reply += (x > 20);
+    reply += "."
     document.getElementById("theBox").innerHTML = reply;
+    console.log("Finished Booleans" + (2 + 2) + (5 < 3));
+}
+
+function write(location, text) {
+    let p = "";
+    p += location;
+    document.getElementById(p).innerHTML = text;
+}
+
+function madness() {
+    let a = 5;
+    let b = "5";
+    let c = 7;
+    let d = false;
+
+    write("sameTypeData", a === a);
+    write("differentBoth", a === d);
+    write("sameType", a === c);
+    write("sameData", a === b);
+
 }
