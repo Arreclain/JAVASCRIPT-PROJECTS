e973b5b1fc8661cfa15ba22ae7eefff8f09b4c2e
Arreclain <arreclain@gmail.com>
2025-05-22 15:35:48 -0700

    Added CSS and some functionality.

--- Basic JavaScript Projects/Project5_comparisons_type_coersion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coersion/JS/main.js
@@ -8,4 +8,23 @@ document.getElementById("bResults").innerHTML = "A number plus a string yields a
 //document.write(typeof a);
 //document.write(typeof b);
 
+document.getElementById("naanIsBread").innerHTML = 0/0;
+document.getElementById("correct").innerHTML = isNaN("Run");
+document.getElementById("incorrect").innerHTML = isNaN(3);
+
+document.getElementById("toInfinity").innerHTML = 3E320;
+document.getElementById("andBeyond").innerHTML = -3E320;
+}
+
+function booleans(){
+    let x = 16;
+    part1 = false;
+    part2 = false;
+    reply = "Well..."
+
+    reply+= (x%2 == 0);
+    reply+=" and "
+    reply+= (x>20);
+    reply+="."
+    document.getElementById("theBox").innerHTML = reply;
 }
