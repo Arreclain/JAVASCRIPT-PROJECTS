26cd8a58d27dfea7c35488bab36243afaf72bd52
Arreclain <arreclain@gmail.com>
2025-05-22 12:12:49 -0700

    Implemented an instructor's help.

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,10 @@
 function my_Dictionary(){
     var ShipClass = {
-        patrol: destroyer,
-        skirmish: corvette,
-        raid: frigate,
-        battle: cruiser,
-        war: battleship
+        patrol: "destroyer",
+        skirmish: "corvette",
+        raid: "frigate",
+        battle: "cruiser",
+        war: "battleship"
     };
     document.getElementById("Dictionary").innerHTML = ShipClass.patrol;
 }
