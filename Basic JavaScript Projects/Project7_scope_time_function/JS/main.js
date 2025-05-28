0d3356c692115a7a0c4ac566efcc8f58699695d6
Arreclain <arreclain@gmail.com>
2025-05-28 14:57:21 -0700

    Added a function as per requirements.

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -12,34 +12,64 @@ function bakery() {
     bakeBread();
     bakeBread();
     shipBread();
-    document.getElementById("breadReport").innerHTML = "Milan has " + townBread +" loaves of bread from this one bakery.";
+    document.getElementById("breadReport").innerHTML = "Milan has " + townBread + " loaves of bread from this one bakery.";
 }
 
-function updateTime(){
+function updateTime() {
     const hours = new Date().getHours();
     const minutes = new Date().getMinutes();
     document.getElementById("clock").innerHTML = "The local time is: " + hours + ":" + minutes;
-//    alert("Event triggered.");
+    //    alert("Event triggered.");
 }
-    var minutes = 2;
-    var seconds = 0;
 
-function startClock(){
-    alert("A timer has started");
-    //setInterval(decriment,1000);
-    /*function decriment(){
-        seconds--;
-        if (seconds < 0){
-            seconds = 59;
-            if(minutes < 0){
-                seconds = 0;
-                minutes = 0;
-                alert("You ran out of time.");
-                return null;
-            }
-            minutes--;
+function timeFunction(){
+    var time = new Date().getHours();
+    var output = "error";
+
+        if(time < 12 == time > 6){
+            output = "It's the morning."; 
         }
-        document.getElementById("countdownTimer").innerHTML = minutes + ":" + seconds;
-    } */
-
+        else if( time >=12 && time < 16){
+            output = "It's the afternoon.";
+        }
+        else if(time >=16 && time < 21){
+            output = "It's the evening.";
+        }
+        else{
+            output = "Go to bed.";
+        }
+        document.getElementById("Time_of_Day").innerHTML = output;
 }
+
+
+var minutes = 2;
+var seconds = 0;
+const wire = Math.floor(Math.random()*10 + 1);
+const correctWire = "wire" + wire;
+var clockItself;
+
+function startClock() {
+    //alert("A timer has started");
+    clockItself = setInterval(decrement, 1000);
+//    console.info(wire);
+}
+
+function decrement() {
+       if (document.getElementById(correctWire).checked == true) {
+           clearInterval(clockItself);
+           alert("You stopped the clock.  You're a hero!  I guess?");
+       } 
+    seconds--;
+    if (seconds < 0) {
+        seconds = 59;
+        minutes--;
+        if (minutes < 0) {
+            seconds = 0;
+            minutes = 0;
+            alert("You ran out of time.  The correct choice was "+ correctWire);
+            clearInterval(clockItself);
+            return;
+        }
+    }
+    document.getElementById("countdownTimer").innerHTML = minutes + ":" + seconds;
+} 
