dfaf6e969d4206dac4f01a3add60b753210b1dda
Arreclain <arreclain@gmail.com>
2025-06-24 14:03:18 -0700

    commented my code

--- JQuery/JS/script.js
+++ JQuery/JS/script.js
@@ -17,4 +17,30 @@ function shineLoop(){
                 $("h1").removeClass("header-scrolled");
             }
         });
+
+        $("#classicCars").on({
+            //Triggers the images to slide down one after the other
+            //when the user's mouse enters the div area of the specified ID
+            mouseenter: function() {
+                $("#titleOne").show(1000);
+                $("#titleTwo").show(1500);
+                $("#titleThree").show(2000);
+            },
+
+            //Triggers the images to slide up one after the other when
+            //the user's mouse leaves the div area of the specified ID.
+            mouseleave: function() {
+                $("#titleOne").hide(2000);
+                $("#titleTwo").hide(1500);
+                $("#titleThree").hide(1000);
+            }
+        });
+
+            //Creates a toggle effect on each FAQ question by checking
+            //each element's sibling so they don't all display when
+            //one question is clicked.
+        $('div.question').on('click', function(){
+            $(this).next().slideToggle('slow'); //Slow = 600 miliseconds or 600 when in JS.
+        });
+
 })
